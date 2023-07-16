fe49374627e22642634b9c40c650edf239c75f9b
Erin Stephens <erebstephens@gmail.com>
2023-07-16 13:51:24 -0500

    setup views for each entity

---
+++ bangazonapi/views/product.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponseServerError
+from django.db.models import Count
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers, status
+
+class ProductView(ViewSet):
+
+    def retrieve(self, request, pk):
+    
+    
+    def list(self, request):
