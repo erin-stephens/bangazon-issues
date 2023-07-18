e7595a6c4f410d74a7b8c6fd466076480a1945d3
Erin Stephens <erebstephens@gmail.com>
2023-07-17 21:11:06 -0500

    added views for products and orders

--- bangazonapi/views/order.py
+++ bangazonapi/views/order.py
@@ -3,10 +3,52 @@ from django.db.models import Count
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
+from bangazonapi.models import Order, User
 
 class OrderView(ViewSet):
 
     def retrieve(self, request, pk):
     
+        try: 
+            order = Order.objects.get(pk=pk)
+            serializer = OrderSerializer(order)
+            return Response(serializer.data)
+        except Order.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
     
     def list(self, request):
+        orders = Order.objects.all()
+        serializer = OrderSerializer(orders, many=True)
+        return Response(serializer.data)
+        
+    def create(self, request):
+        customer = User.objects.get(pk=request.data["userId"])
+        order = Order.objects.create(
+            customer=customer,
+            completed = request.data["completed"],
+            total = request.data["total"],
+            payment_type = request.data["paymentType"]
+        )
+        serializer = OrderSerializer(order)
+        return Response(serializer.data)
+    
+    def update(self, request, pk):
+        order = Order.objects.get(pk=pk)
+        customer = User.objects.get(pk=request.data["userId"])
+        order.customer=customer
+        order.completed = request.data["completed"]
+        order.total = request.data["total"]
+        order.payment_type = request.data["paymentType"]
+        order.save()
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
+    
+    def destroy(self, request, pk):
+        
+        order = Order.objects.get(pk=pk)
+        order.delete()
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = ('id', 'customer', 'total', 'payment_type')
--- bangazonapi/views/product.py
+++ bangazonapi/views/product.py
@@ -3,10 +3,66 @@ from django.db.models import Count
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
+from bangazonapi.models import Product, User, Category
 
 class ProductView(ViewSet):
 
     def retrieve(self, request, pk):
+        
+        product = Product.objects.get(pk=pk)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
     
     
     def list(self, request):
+        
+        products = Product.objects.all()
+        seller_products = request.query_params.get('user_id', None)
+        if seller_products is not None:
+            products = products.filter(user_id=seller_products)
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+    
+    def create(self, request):
+        
+        seller = User.objects.get(pk=request.data["userId"])
+        category = Category.objects.get(pk=request.data["categoryId"])
+        product = Product.objects.create(
+            seller_id=seller,
+            category_id=category,
+            title=request.data["title"],
+            description=request.data["description"],
+            quantity=request.data["quantity"],
+            price=request.data["price"],
+            image_url=request.data["imageUrl"]
+        )
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+        
+    def update(self, request, pk):
+        
+        product = Product.objects.get(pk=pk)
+        seller = User.objects.get(pk=request.data["userId"])
+        product.seller = seller
+        category = Category.objects.get(pk=request.data["categoryId"])
+        product.category=category
+        product.title = request.data["title"]
+        product.description = request.data["description"]
+        product.quantity = request.data["quantity"]
+        product.price=request.data["price"]
+        product.image_url=request.data["imageUrl"]
+        product.save()
+        
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
+    
+    def destroy(self, request, pk):
+        
+        product = Product.objects.get(pk=pk)
+        product.delete()
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
+
+class ProductSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Product
+        fields = ('id', 'seller', ' category', 'title', 'description', 'quantity', 'price', 'image_url')
